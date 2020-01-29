e6072352ff6729eacca2915599185ba97b5c1b0a
Blaine Hussey <blainehussey@protonmail.ch>
2020-01-29 11:51:33 -0500

    Installed gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/samples`,
+        path: `${__dirname}/src/samples/`,
         name: `samples`,
       },
     },
