c712ca2b7b107d6378248be3a904ac88147b271e
Blaine Hussey <blainehussey@protonmail.ch>
2020-01-29 12:45:01 -0500

    Deleted Gatsby default images, moved CSS to new folder, and added Footer component

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const Footer = () => (
+  <footer>
+    © {new Date().getFullYear()}, Built with
+    {` `}
+    <a href="https://www.gatsbyjs.org">Gatsby</a>
+  </footer>
+)
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import Footer from "./footer"
+import "../css/layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -34,11 +35,7 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <Footer/>
       </div>
     </>
   )
