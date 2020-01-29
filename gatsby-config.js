89f091929caed7a8c013af5fd30c8e67292ea384
Blaine Hussey <blainehussey@protonmail.ch>
2020-01-29 09:55:22 -0500

    gatsby-source-filesystem path updated and added gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,11 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/samples`,
+        name: `samples`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
