881f460206eb947ae66dff948952f0aee3e38664
Blaine Hussey <blainehussey@protonmail.ch>
2020-02-06 11:54:29 -0500

    Rebuilt index.js - reformatted

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,32 +2,34 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default ({ data }) => (
-  <Layout>
-    {data.allMarkdownRemark.edges.map(({ node }) => (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm">
-            <div className="card">
-              <a href={node.fields.slug}>
-                <div className="card-header">
-                  <img src="" alt=" " />
-                  <h3 className="card-title">{node.frontmatter.title}</h3>
+const SamplesIndex = ({ data, location }) => {
+  const samples = data.allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      {samples.map(({ node }) => (
+        <div className="col-4">
+          <div className="card">
+            <a href={node.fields.slug}>
+              <div className="card-header">
+                <img src="" alt=" " />
+                <h3 className="card-title">{node.frontmatter.title}</h3>
+              </div>
+              <div className="card-body">
+                <div className="tutorial-description">
+                  {node.frontmatter.summary}
                 </div>
-                <div className="card-body">
-                  <div className="tutorial-description">
-                    {node.frontmatter.summary}
-                  </div>
-                </div>
-                <div className="card-footer">Start tutorial</div>
-              </a>
-            </div>
+              </div>
+              <div className="card-footer">Start tutorial</div>
+            </a>
           </div>
         </div>
-      </div>
-    ))}
-  </Layout>
-)
+      ))}
+    </Layout>
+  )
+}
+
+export default SamplesIndex
 
 export const query = graphql`
   {
