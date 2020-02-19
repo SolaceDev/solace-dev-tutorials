7510b671b16d50e01ad44e8a8661f13a7fdd2bcb
Ghaith Dalla-Ali <ghaith.dalla-ali@solace.com>
2020-02-19 09:55:45 -0500

    DATAGO-6884: Port Solace Samples to Gatsby -- header anchors, code syntax/styling, md fragments  (#4)
    * added and configured gatsby-remark-autolink-headers (for heading anchors and div IDs), and gatsby-remark-prismjs (for code syntax highlighting)
    
    * Make use of gatsby-remark-embed-markdown in only the javascript's request-reply tutorial (for now), to embed md fragments into a parent md
    
    * Added a breadcrumb and a table of content in the tutorial pages

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
-import "./node_modules/bootstrap/dist/css/bootstrap.css"
-import "./src/css/global.css"
+// module styles
+import "./node_modules/bootstrap/dist/css/bootstrap.css";
+import "prismjs/themes/prism.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+
+// user styles
+import "./src/css/global.css";
+import "./src/css/prismjs.css";
+import "./src/css/breadcrumb.css"
