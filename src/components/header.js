c1cbc757490a33fbc55c74d57ff5c588e11438bf
Blaine Hussey <blainehussey@protonmail.ch>
2020-04-08 16:20:22 -0400

    Fix: Updated logo and links in Navbar, styling needed

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,30 @@
 import PropTypes from "prop-types"
 import React from "react"
 import { Navbar, Nav } from "react-bootstrap"
-import solaceDevLogo from "../images/solace-dev-logo-white.png"
+import solaceDevLogo from "../images/solace-developers-logo-white.png"
 
 const Header = ({ siteTitle }) => (
   <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
     <Navbar.Brand href="https://solace.com/">
-      <a target="_blank" rel="noopener noreferrer" href="https://solace.dev">
-        <img src={solaceDevLogo} alt="Solace Developers Logo" width="300px" />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.solace.dev"
+      >
+        <img src={solaceDevLogo} alt="Solace Developers Logo" />
       </a>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
       <Nav>
-        <Nav.Link href="https://solace.com/products/event-broker/cloud/">
-          Features
+        <Nav.Link href="https://solace.com/try-it-now/">
+          Getting Started
         </Nav.Link>
-        <Nav.Link href="https://solace.com/products/event-broker/cloud/pricing/">
-          Pricing
-        </Nav.Link>
+        <Nav.Link href="https://solace.community/">Community</Nav.Link>
+        <Nav.Link href="https://docs.solace.com/">Docs</Nav.Link>
+        <Nav.Link href="https://solace.com/downloads/">Downloads</Nav.Link>
         <Nav.Link href="https://solace.com/learn/">Learn</Nav.Link>
-        <Nav.Link href="https://solace.com/contact/">Talk to Sales</Nav.Link>
-        <Nav.Link href="https://solace.com/try-it-now/">Try PubSub+</Nav.Link>
-        <Nav.Link href="https://console.solace.cloud/login/">Log In</Nav.Link>
+        <Nav.Link href="https://console.solace.cloud/login">Log In</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
