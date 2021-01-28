ae471311e669cc42e37147d42debe516717cab83
Blaine Hussey <blainehussey@protonmail.ch>
2021-01-27 21:02:19 -0500

    Fix: FontAwesome CSS rendering issue

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css"
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false /* eslint-disable import/first */
 import "../css/layout.css"
 import Resources from "./resources"
 import Header from "./header"
