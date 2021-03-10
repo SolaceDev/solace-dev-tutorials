6b2ee08fcfbe872d4a3253e8c7bcb2ef51f69795
Blaine Hussey <blainehussey@protonmail.ch>
2021-03-10 09:47:22 -0500

    Removed TODO on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import notFound from "../images/illustrations/undraw_page_not_found_su7k.svg"
 
-// TODO fix styling for 404, etc.
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Page Not Found" />
