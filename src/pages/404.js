83200dbc211a22110e3e4991be483241e8d7f69d
Blaine Hussey <blainehussey@protonmail.ch>
2021-03-10 09:46:39 -0500

    Quick update to the default 404 page, needs more styling and thought

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,20 @@
 import React from "react"
-
+import { Col, Container, Row } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import notFound from "../images/illustrations/undraw_page_not_found_su7k.svg"
 
 // TODO fix styling for 404, etc.
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404: Page Not Found" />
+    <Container className="mt5 mb5">
+      <Row>
+        <Col>
+          <img src={notFound} alt="404: Page Not Found" />
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 )
 
