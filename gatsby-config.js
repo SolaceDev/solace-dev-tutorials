6b8f8b8b27b70b06e1823f504abad62225899d6c
Blaine Hussey <blainehussey@protonmail.ch>
2022-04-04 15:28:49 -0400

    Removed GTM package

--- gatsby-config.js
+++ gatsby-config.js
@@ -247,20 +247,5 @@ module.exports = {
         useClassNames: true,
       },
     },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-KDMNNWW",
-
-        // Include GTM in development.
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
-      },
-    },
   ],
 }
