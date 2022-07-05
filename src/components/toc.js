86b9b776de2443c1fec8ca2299e06da3f674938f
Blaine Hussey <blainehussey@protonmail.ch>
2022-07-05 11:47:52 -0400

    Fixed missing IDs in H2s from includes

--- src/components/toc.js
+++ src/components/toc.js
@@ -1,32 +1,27 @@
 import React from "react"
-import {Col, Button} from "react-bootstrap"
-import {faGithub} from "@fortawesome/free-brands-svg-icons"
-import {faComments} from "@fortawesome/free-solid-svg-icons"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { Col, Button } from "react-bootstrap"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faComments } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   TwitterShareButton,
   FacebookShareButton,
   LinkedinShareButton,
   TwitterIcon,
   FacebookIcon,
-  LinkedinIcon
+  LinkedinIcon,
 } from "react-share"
 
 const TableOfContent = (props) => {
   let headings = props.headings
   let feedback_link = props.feedback_link
-  let link = `https://tutorials.solace.dev${
-    props.slug
-  }`
-  let social_string = `I am learning ${
-    props.page_title
-  } for ${
-    props.slugRoot
-  } on the Solace API Tutorials page! Check it out here 👇 🤓 \n`
+  let link = `https://tutorials.solace.dev${props.slug}`
+  let social_string = `I am learning ${props.page_title} for ${props.slugRoot} on the Solace API Tutorials page! Check it out here 👇 🤓 \n`
   let hashtags = ["solace", "pubsub", "tutorial", "DEVCommunity"]
   return (
   
     <div id="sidebar">
+<<<<<<< HEAD
     <div className="heading">Table of content</div>
     <Col> {
       headings.map((heading) => (<div key={
@@ -94,6 +89,71 @@ function updateIDs(headings) {
   // h2elms.map((elm) => {
   // console.log(elm.accessKey)
   // })
+=======
+      <div className="heading">Table of Contents</div>
+      <Col>
+        {" "}
+        {headings.map((heading) => (
+          <div key={heading.id} className="pt2 pb2 border-bottom">
+            <a href={"#" + heading.value.split(" ").join("-")}>
+              {heading.value}{" "}
+            </a>
+          </div>
+        ))}{" "}
+      </Col>
+      <Col>
+        <Button
+          className="mt3 mb2 w-100"
+          href={feedback_link}
+          target="_blank"
+          rel="noreferrer"
+          variant="primary"
+          size="sm"
+        >
+          <FontAwesomeIcon icon={faGithub} size="1x" />
+          Improve this page{" "}
+        </Button>{" "}
+        <Button
+          className="mt1 mb3 w-100"
+          href="https://solace.community/"
+          target="_blank"
+          rel="noreferrer"
+          variant="primary"
+          size="sm"
+        >
+          <FontAwesomeIcon icon={faComments} size="1x" />
+          Discuss in Community{" "}
+        </Button>{" "}
+      </Col>
+      <Col className="social">
+        <TwitterShareButton
+          className="pr-1 pl-1"
+          url={link}
+          title={social_string}
+          hashtags={hashtags}
+          via="SolaceDevs"
+        >
+          <TwitterIcon size={32} round={true} />
+        </TwitterShareButton>
+        <LinkedinShareButton
+          className="pr-1 pl-1"
+          url={link}
+          summary={social_string}
+        >
+          <LinkedinIcon size={32} round={true} />
+        </LinkedinShareButton>
+        <FacebookShareButton
+          className="pr-1 pl-1"
+          url={link}
+          quote={social_string}
+          hashtags={hashtags}
+        >
+          <FacebookIcon size={32} round={true} />
+        </FacebookShareButton>
+      </Col>
+    </div>
+  )
+>>>>>>> b35fa4a (Fixed missing IDs in H2s from includes)
 }
 
 export default TableOfContent
--- src/templates/tutorial.js
+++ src/templates/tutorial.js
@@ -1,30 +1,47 @@
 import React from "react"
-import {graphql} from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Sidebar from "../components/sidebar"
 import TableOfContent from "../components/toc"
-import {Col, Container, Row} from "react-bootstrap"
-import {Breadcrumb} from "gatsby-plugin-breadcrumb"
+import { Col, Container, Row } from "react-bootstrap"
+import { Breadcrumb } from "gatsby-plugin-breadcrumb"
 import SEO from "../components/seo"
 
-const tutorial = ({data, pageContext}) => {
-  const {breadcrumb: {
-      crumbs
-    }} = pageContext
+const tutorial = ({ data, pageContext }) => {
+  const {
+    breadcrumb: { crumbs },
+  } = pageContext
   const solaceDevCrumb = {
     pathname: "https://solace.dev/",
-    crumbLabel: "Developer Hub"
+    crumbLabel: "Developer Hub",
   }
   // Clone the crumbs object
   const crumbs_clone = crumbs.slice()
   crumbs_clone.unshift(solaceDevCrumb)
 
   const node = data.tutorialBody.edges[0].node
-  const tutorials = data.tableOfContent.edges.filter((edge) => edge.node.frontmatter.layout === "tutorials" && edge.node.frontmatter.visible !== false)
-  console.log("Page Headings")
+  const tutorials = data.tableOfContent.edges.filter(
+    (edge) =>
+      edge.node.frontmatter.layout === "tutorials" &&
+      edge.node.frontmatter.visible !== false
+  )
   const headings = node.headings.filter((heading) => heading.depth == 2)
-  const features = data.tableOfContent.edges.filter((edge) => edge.node.frontmatter.layout === "features" && edge.node.frontmatter.visible !== false)
-  const feedback = node.frontmatter.links.filter((link) => link.label === "feedback")
+  const headersWithNullIds = headings.filter((elm) => elm.id == null)
+  // Make sure there is IDs for every h2 to be used in hrefs of TOC
+  let modified_html = node.html
+  headersWithNullIds.map((heading) => {
+    modified_html = node.html.replace(
+      `<h2>${heading.value}</h2>`,
+      `<h2 id="${heading.value.split(" ").join("-")}">${heading.value}</h2>`
+    )
+  })
+  const features = data.tableOfContent.edges.filter(
+    (edge) =>
+      edge.node.frontmatter.layout === "features" &&
+      edge.node.frontmatter.visible !== false
+  )
+  const feedback = node.frontmatter.links.filter(
+    (link) => link.label === "feedback"
+  )
   const feedback_link = feedback[0].link || ""
   const section_title = data.allTutorialsYaml.edges[0].node.section1
   const page_title = node.frontmatter.title
@@ -33,61 +50,37 @@ const tutorial = ({data, pageContext}) => {
 
   return (
     <Layout hideResources="true">
-      <SEO title={
-        node.frontmatter.title
-      }/>
+      <SEO title={node.frontmatter.title} />
       <section id="breadcrumbs">
         <Container>
-          <Breadcrumb crumbs={crumbs_clone}
-            crumbLabel={
-              node.frontmatter.title
-            }
-            crumbSeparator=" 👉 "/>
+          <Breadcrumb
+            crumbs={crumbs_clone}
+            crumbLabel={node.frontmatter.title}
+            crumbSeparator=" 👉 "
+          />
         </Container>
       </section>
       <Container className="mt4 pb4">
         <Row>
-          <Col xs={12}
-            sm={12}
-            md={8}
-            lg={9}
-            xl={9}>
-            <h1>{
-              node.frontmatter.title
-            }</h1>
+          <Col xs={12} sm={12} md={8} lg={9} xl={9}>
+            <h1>{node.frontmatter.title}</h1>
             <h5 id="minutes" className="mb3 pt2">
-              {
-              node.timeToRead
-            }
+              {node.timeToRead}
               Minute Read
             </h5>
-            <div dangerouslySetInnerHTML={
-              {__html: node.html}
-            }/>
+            <div dangerouslySetInnerHTML={{ __html: modified_html }} />
           </Col>
-          <Col xs={12}
-            sm={12}
-            md={4}
-            lg={3}
-            xl={3}>
-
-            {/* <Sidebar features={features}
-              tutorials={tutorials}
-              feedback_link={feedback_link}
-              section_title={section_title}
-              pageContext={pageContext}
-              page_title={page_title}
-              slug={slug}
-              slugRoot={slugRoot}></Sidebar> */}
-
-            <TableOfContent features={features}
+          <Col xs={12} sm={12} md={4} lg={3} xl={3}>
+            <TableOfContent
+              features={features}
               headings={headings}
               pageContext={pageContext}
               feedback_link={feedback_link}
               section_title={section_title}
               page_title={page_title}
               slug={slug}
-              slugRoot={slugRoot}></TableOfContent>
+              slugRoot={slugRoot}
+            ></TableOfContent>
           </Col>
         </Row>
       </Container>
@@ -95,7 +88,7 @@ const tutorial = ({data, pageContext}) => {
   )
 }
 
-export const query = graphql `
+export const query = graphql`
   query myTutorialQuery($slug: String, $slugRoot: String) {
     tutorialBody: allMarkdownRemark(
       filter: { fields: { slug: { eq: $slug } } }
