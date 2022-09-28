3e95e209ce2ce784fc24d648cc76e3f3615008ba
Blaine Hussey <blaine.hussey@solace.com>
2022-09-28 17:02:34 -0400

    package-lock.json sync

--- src/components/toc.js
+++ src/components/toc.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 import { Col, Button } from "react-bootstrap"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { faComments } from "@fortawesome/free-solid-svg-icons"
@@ -12,17 +12,17 @@ import {
   LinkedinIcon,
 } from "react-share"
 
-const TableOfContent = (props) => { 
+const TableOfContent = (props) => {
   let headings = props.headings
   let feedback_link = props.feedback_link
   let link = `https://tutorials.solace.dev${props.slug}`
   let social_string = `I am learning ${props.page_title} for ${props.slugRoot} on the Solace API Tutorials page! Check it out here 👇 🤓 \n`
   let hashtags = ["solace", "pubsub", "tutorial", "DEVCommunity"]
 
-  const [visibleHeader, setVisibleHeader] = useState(null);
-  const handleObserverEvent = entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) { 
+  const [visibleHeader, setVisibleHeader] = useState(null)
+  const handleObserverEvent = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
         setVisibleHeader(entry.target.id.split("-").join(" "))
       }
     })
@@ -31,13 +31,13 @@ const TableOfContent = (props) => {
   useEffect(() => {
     let options = {
       root: null,
-      rootMargin: '0px',
-      threshold: 1.0
+      rootMargin: "0px",
+      threshold: 1.0,
     }
     let observer = new IntersectionObserver(handleObserverEvent, {})
     const h2s = document.getElementsByTagName("h2")
     var elms = Array.prototype.slice.call(h2s)
-    elms.map(elm => {
+    elms.map((elm) => {
       observer.observe(elm)
     })
     return () => {
@@ -48,60 +48,78 @@ const TableOfContent = (props) => {
 
   return (
     <div id="sidebar">
-    <div className="heading">Table of content</div>
-    <Col> {
-      headings.map((heading) => (<div key={
-          heading.id
-        }
-        style={{ background: visibleHeader == heading.value? 'red': 'blue'}}
-        className="pt2 pb2 border-bottom">
-        <a href={
-          "#" + heading.value.split(" ").join("-").replace(/:/g, '')
-        }> {
-          heading.value
-        } </a>
-      </div>))
-    } </Col>
-    <Col>
-      <Button className="mt3 mb2 w-100"
-        href={feedback_link}
-        target="_blank"
-        rel="noreferrer"
-        variant="primary"
-        size="sm">
-        <FontAwesomeIcon icon={faGithub}
-          size="1x"/>
-        Improve this page{" "} </Button>
-      {" "}
-      <Button className="mt1 mb3 w-100" href="https://solace.community/" target="_blank" rel="noreferrer" variant="primary" size="sm">
-        <FontAwesomeIcon icon={faComments}
-          size="1x"/>
-        Discuss in Community{" "} </Button>
-      {" "} </Col>
-    <Col className="social">
-      <TwitterShareButton className="pr-1 pl-1"
-        url={link}
-        title={social_string}
-        hashtags={hashtags}
-        via="SolaceDevs">
-        <TwitterIcon size={32}
-          round={true}/>
-      </TwitterShareButton>
-      <LinkedinShareButton className="pr-1 pl-1"
-        url={link}
-        summary={social_string}>
-        <LinkedinIcon size={32}
-          round={true}/>
-      </LinkedinShareButton>
-      <FacebookShareButton className="pr-1 pl-1"
-        url={link}
-        quote={social_string}
-        hashtags={hashtags}>
-        <FacebookIcon size={32}
-          round={true}/>
-      </FacebookShareButton>
-    </Col>
-  </div>)
+      <div className="heading">Table of content</div>
+      <Col>
+        {" "}
+        {headings.map((heading) => (
+          <div
+            key={heading.id}
+            style={{
+              background: visibleHeader === heading.value ? "#000" : " ",
+            }}
+            className="pt2 pb2 border-bottom"
+          >
+            <a
+              href={"#" + heading.value.split(" ").join("-").replace(/:/g, "")}
+            >
+              {" "}
+              {heading.value}{" "}
+            </a>
+          </div>
+        ))}{" "}
+      </Col>
+      <Col>
+        <Button
+          className="mt3 mb2 w-100"
+          href={feedback_link}
+          target="_blank"
+          rel="noreferrer"
+          variant="primary"
+          size="sm"
+        >
+          <FontAwesomeIcon icon={faGithub} size="1x" />
+          Improve this page{" "}
+        </Button>{" "}
+        <Button
+          className="mt1 mb3 w-100"
+          href="https://solace.community/"
+          target="_blank"
+          rel="noreferrer"
+          variant="primary"
+          size="sm"
+        >
+          <FontAwesomeIcon icon={faComments} size="1x" />
+          Discuss in Community{" "}
+        </Button>{" "}
+      </Col>
+      <Col className="social">
+        <TwitterShareButton
+          className="pr-1 pl-1"
+          url={link}
+          title={social_string}
+          hashtags={hashtags}
+          via="SolaceDevs"
+        >
+          <TwitterIcon size={32} round={true} />
+        </TwitterShareButton>
+        <LinkedinShareButton
+          className="pr-1 pl-1"
+          url={link}
+          summary={social_string}
+        >
+          <LinkedinIcon size={32} round={true} />
+        </LinkedinShareButton>
+        <FacebookShareButton
+          className="pr-1 pl-1"
+          url={link}
+          quote={social_string}
+          hashtags={hashtags}
+        >
+          <FacebookIcon size={32} round={true} />
+        </FacebookShareButton>
+      </Col>
+    </div>
+  )
 }
 
 export default TableOfContent
