6b58a2646a6fa863d75d525a6aaf4ca74a595df6
Tamimi Ahmad <TamimiGitHub@users.noreply.github.com>
2024-09-13 13:46:16 -0700

    Create eda-banner.js (#158)
    * Create eda-banner.js
    
    * Update header.js
    
    * Update layout.css to add eda css
    
    * fix broken build

---
+++ src/components/header/eda-banner.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const EDABanner = props => {
+  return (
+    <div class="eda_banner">
+      <p>
+        Register Now for EDA Summit 2024 - Virtual Event
+        <a href="https://edasummit.com/" target="_blank">
+          Register for Free
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default EDABanner
