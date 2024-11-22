bbf38711a30bc40faa2b234ee8af60309e9608dc
Blaine Hussey <blainehussey@users.noreply.github.com>
2024-11-22 09:25:06 -0500

    Updated link in EDA Summit banner to point to the new Registration page (#159)

--- src/components/header/eda-banner.js
+++ src/components/header/eda-banner.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-const EDABanner = props => {
+const EDABanner = (props) => {
   return (
     <div class="eda_banner">
       <p>
         Register Now for EDA Summit 2024 - Virtual Event
-        <a href="https://edasummit.com/" target="_blank">
+        <a href="https://edasummit.com/events/summit-2024/" target="_blank">
           Register for Free
         </a>
       </p>
