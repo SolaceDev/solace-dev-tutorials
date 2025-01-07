89737a6661588052a417426897d5018707a65290
Tamimi <tamimi@solace.com>
2025-01-07 09:29:02 -0800

    change css

--- src/components/header/lightning-banner.js
+++ src/components/header/lightning-banner.js
@@ -4,7 +4,7 @@ const LightningBanner = (props) => {
   return (
     <div class="lightning_banner">
       <p>
-        The third Solace Community Lightning Talks
+        The Third Solace Community Lightning Talks hosted on January 15, 2025
         <a href="https://www.youtube.com/watch?v=mv4NFa_MPPU" target="_blank">
           Watch Now!
         </a>
