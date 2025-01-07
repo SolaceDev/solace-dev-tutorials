0254c79fb5048a5ff800afb1c010f902b078e4a8
Tamimi Ahmad <TamimiGitHub@users.noreply.github.com>
2025-01-07 09:02:13 -0800

    Create lightning-banner.js

---
+++ src/components/header/lightning-banner.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const LightningBanner = (props) => {
+  return (
+    <div class="lightning_banner">
+      <p>
+        The third Solace Community Lightning Talks
+        <a href="https://www.youtube.com/watch?v=mv4NFa_MPPU" target="_blank">
+          Watch Now!
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default LightningBanner
