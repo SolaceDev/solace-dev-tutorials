e6bb9cfa48f6ef709cc94c96a59fb7557fbbc8ad
Blaine Hussey <blainehussey@protonmail.ch>
2025-01-10 14:56:26 -0500

    Updated css for font size and bg colours

--- src/components/header/lightning-banner.js
+++ src/components/header/lightning-banner.js
@@ -1,14 +1,20 @@
 import React from "react"
+import { Container } from "react-bootstrap"
 
 const LightningBanner = (props) => {
   return (
     <div class="lightning_banner">
-      <p>
-        Join the 3rd Edition Solace Community Lighting Talks on January 15
-        <a href="https://solace.com/event/community-lightning-talks-jan-2025/" target="_blank">
-          Learn More
-        </a>
-      </p>
+      <Container>
+        <p>
+          Join the 3rd Edition Solace Community Lighting Talks on January 15
+          <a
+            href="https://solace.com/event/community-lightning-talks-jan-2025/"
+            target="_blank"
+          >
+            Learn More
+          </a>
+        </p>
+      </Container>
     </div>
   )
 }
