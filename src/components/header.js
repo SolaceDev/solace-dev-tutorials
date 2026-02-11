784c41e3ced673cdb8cf6c1f3d83b2787bc3578f
Maryam Lim-Baig <maryam.lim-baig@solace.com>
2026-02-11 16:13:02 -0500

    Updated header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,5 @@
 import PropTypes from "prop-types"
 import React from "react"
-import { Container, Navbar, Nav } from "react-bootstrap"
-import solaceDevLogo from "../images/solace-developers-logo-white.svg"
 import NavBar from "./nav-main"
 // import EDABanner from "./header/eda-banner"
 // import LightningBanner from "./header/lightning-banner"
@@ -10,7 +8,7 @@ const Header = ({ siteTitle }) => (
   <>
     {/* <EDABanner /> */}
     {/* <LightningBanner /> */}
-    <Navbar />
+    <NavBar />
   </>
 )
 
